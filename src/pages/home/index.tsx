65e5fd0cf7c2d41ab88b5f6e45942978ee3038ce
azschokke <alicen.zschokke@gmail.com>
2021-06-09 01:28:32 -0500

    added image

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Jumbotron } from "reactstrap";
+import logo from './kelly.jpg';
 
 const Home = () => {
   return (
     <Container>
       <Jumbotron>
-        <h1>Welcome to Kelly Zschokke's graduation party!</h1>
+        <h1>Welcome to Kelly's graduation party!</h1>
+        <img src={logo} alt="Kelly" height="75%" width="75%"/>
+        <br />
         <Link className="btn btn-primary" to="/trivia">
-          See how well you know kelly
+          See how well you know Kelly!
         </Link>
       </Jumbotron>
     </Container>
